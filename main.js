eaf8245974797203e10be3ed33fe63569bb2b682
Esther Ulysse <estherulysse@Esthers-MBP.fios-router.home>
2021-10-13 21:33:43 -0400

    completed assignment

--- main.js
+++ main.js
@@ -2,6 +2,62 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+// ===========================================
+function isAdmin(object) {
+    if (object["userRole"] === "ADMIN"){
+        return true;
+    } else {
+        return false;
+    }
+}
+// ===========================================
+function getEmail(user) {
+    const firstName = user.firstName.toLowerCase();
+    const lastName = user.lastName.toLowerCase();
+    const email = firstName + '.' + lastName + '@codeimmersives.com';
+    return email;
+}
+
+// ===========================================
+function getPlaylistLength(playlist) {
+    PlaylistLength = playlist.songs.length;
+    return PlaylistLength;
+}
+// ============================================
+function getHardestHomework (homework) {
+    let lowest = 90
+    let hardest = ''
+
+    for (let i =0; i <homework.length; i++) {
+        const score = homework[i].averageScore;
+        
+        if (score < lowest) {
+            lowest =score
+            hardest = homework[i].name
+        }
+    }
+    return hardest;
+}
+// ================================================
+function createPhonebook(nameArr, numArr) {
+    const phoneBook = {};
+    for (let i=0; i < nameArr.length; i++) {
+        let fname = nameArr[i];
+        let phone = numArr[i];
+
+        phoneBook[fname] = phone;
+    }
+    return phoneBook;
+}
+
+
+
+
+
+
+
+
+
 
 
 // ┌─────────────────────────────────────┐
